640949ab414017e2ceda0ba110ad1d6726fada27
JPtheDash <137363790+JPtheDash@users.noreply.github.com>
2025-02-21 09:22:40 +0530

    Create MenuItemRepository.java

---
+++ src/main/java/com/menuqr/repository/MenuItemRepository.java
@@ -0,0 +1,9 @@
+package com.menuqr.repository;
+
+import com.menuqr.model.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+}
