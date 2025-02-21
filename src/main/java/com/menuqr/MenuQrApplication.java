9f02839a92910ad0ee7d60e7841b093a30d0b278
JPtheDash <137363790+JPtheDash@users.noreply.github.com>
2025-02-21 09:16:12 +0530

    Create MenuQrApplication.java

---
+++ src/main/java/com/menuqr/MenuQrApplication.java
@@ -0,0 +1,11 @@
+package com.menuqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuQrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MenuQrApplication.class, args);
+    }
+}
